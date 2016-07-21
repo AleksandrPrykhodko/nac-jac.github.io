6656d87ae8f93db5e26a466e672f8e692e45c189
Aleksandr Prykhodko <2aleks2@gmail.com>
2016-07-21 21:58:48 +0300

    initial structure

---
+++ app/controllers/mail_controller.rb
@@ -0,0 +1,5 @@
+class MailController < ApplicationController
+  def contact
+    render plain: "success"
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+    end
+
+  def terms_of_use
+  end
+
+  def privacy_policy
+  end
+end
