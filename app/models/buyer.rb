fa0cdf6ae7f84afc26e0d415749f55d885d6911e
gEndelf <alex.com.ua@gmail.com>
2016-07-23 22:01:34 +0300

    update buyer

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,3 +1,3 @@
 class Buyer < ActiveRecord::Base
-  validates :first_name, :last_name, :email, :address1, :address2, :state, :zip, :country, presence: true
+  validates :first_name, :last_name, :email, :address1, :state, :zip, :country, presence: true
 end
